1d9318edfc70667f89c4cf414094e6adc7cdcc7a
mcarneir0 <matheusfeitosa@outlook.com>
2026-04-22 22:06:28 -0300

    Adicionado Quarkus

---
+++ src/test/java/br/gov/caixa/caixaverso/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.gov.caixa.caixaverso;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
